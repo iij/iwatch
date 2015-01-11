dac4f5237cddc9bb4103f33bbf8c1830887857ac
YASUOKA Masahiko <yasuoka@yasuoka.net>
2015-01-12 01:06:16 +0900

    Fix the argument for wcslcpy.

--- iwatch_test.c
+++ iwatch_test.c
@@ -27,27 +27,27 @@ untabify_test(void)
 {
 	wchar_t buf[80];
 
-	wcslcpy(buf, L"\tOK", sizeof(buf));
+	wcslcpy(buf, L"\tOK", sizeof(buf) / sizeof(wchar_t));
 	watch_untabify(buf, sizeof(buf));
 	ASSERT(wcscmp(buf, L"        OK") == 0);
 
-	wcslcpy(buf, L"    \tOK", sizeof(buf));
+	wcslcpy(buf, L"    \tOK", sizeof(buf) / sizeof(wchar_t));
 	watch_untabify(buf, sizeof(buf));
 	ASSERT(wcscmp(buf, L"        OK") == 0);
 
-	wcslcpy(buf, L"       \tOK", sizeof(buf));
+	wcslcpy(buf, L"       \tOK", sizeof(buf) / sizeof(wchar_t));
 	watch_untabify(buf, sizeof(buf));
 	ASSERT(wcscmp(buf, L"        OK") == 0);
 
 	memset(buf, 0xDD, sizeof(buf));
-	wcslcpy(buf, L"\tOK", sizeof(buf));
+	wcslcpy(buf, L"\tOK", sizeof(buf) / sizeof(wchar_t));
 	watch_untabify(buf, 8 * sizeof(wchar_t));
 	ASSERT(wcsncmp(buf, L"       ", 7) == 0);
 	ASSERT(*((u_char *)&buf[8]) == 0xdd)	/* don't overflow */
 	ASSERT(buf[7] == L'\0');		/* null terminate */
 
 	memset(buf, 0xDD, sizeof(buf));
-	wcslcpy(buf, L"\tOKOK", sizeof(buf));
+	wcslcpy(buf, L"\tOKOK", sizeof(buf) / sizeof(wchar_t));
 	watch_untabify(buf, 11 * sizeof(wchar_t));
 	ASSERT(wcscmp(buf, L"        OK") == 0);
 	ASSERT(*((u_char *)&buf[11]) == 0xdd)	/* don't overflow */
@@ -58,15 +58,15 @@ untabify_test2(void)
 {
 	wchar_t buf[80];
 
-	wcslcpy(buf, L"\t\u4e86\u89e3", sizeof(buf));
+	wcslcpy(buf, L"\t\u4e86\u89e3", sizeof(buf) / sizeof(wchar_t));
 	watch_untabify(buf, sizeof(buf));
 	ASSERT(wcscmp(buf, L"        \u4e86\u89e3") == 0);
 
-	wcslcpy(buf, L"  \t\u4e86\u89e3", sizeof(buf));
+	wcslcpy(buf, L"  \t\u4e86\u89e3", sizeof(buf) / sizeof(wchar_t));
 	watch_untabify(buf, sizeof(buf));
 	ASSERT(wcscmp(buf, L"        \u4e86\u89e3") == 0);
 
-	wcslcpy(buf, L"     \t\u4e86\u89e3", sizeof(buf));
+	wcslcpy(buf, L"     \t\u4e86\u89e3", sizeof(buf) / sizeof(wchar_t));
 	watch_untabify(buf, sizeof(buf));
 	ASSERT(wcscmp(buf, L"        \u4e86\u89e3") == 0);
 }
